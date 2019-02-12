b9979af6d1371e54014887abdfbfa00dd1da5b6c
Kevin <stradtkt22@gmail.com>
2019-02-11 19:38:37 -0500

    Added custom recipes

--- classes/cookbook.php
+++ classes/cookbook.php
@@ -8,29 +8,6 @@
 
 include('Recipe.php');
 include('Render.php');
+include('../inc/recipes.php');
 
-$recipe1 = new Recipe("My First Recipe");
-$recipe1->setSource("Patrick Stradtman");
-$recipe1->addIngredient("egg", 1, "oz");
-$recipe1->addIngredient("flour", 2, "cup");
 
-$recipe2 = new Recipe("Pumpkin Pie");
-$recipe2->setSource("Debra Kellington");
-$recipe2->addIngredient("milk", 1, "pint");
-
-echo $recipe1->getTitle();
-foreach ($recipe1->getIngredients() as $ing)
-{
-    echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
-}
-echo implode("\n", $recipe1->getInstructions());
-$recipe1->addTag("Breakfast");
-$recipe1->addTag("Main Course");
-
-echo implode(", ", $recipe1->getTags());
-
-$recipe1->setYield("6 servings");
-echo $recipe1->getYield();
-echo $recipe1->getSource();
-
-Render::displayRecipe($recipe1);
